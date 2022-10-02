fdb6787f54be948845ec2702351dbf8bfc27836c
rickxz <herick.victor.rodrigues@hotmail.com>
2022-10-02 02:36:47 -0300

    feat: criação do arquivo notas.csv caso não exista

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import schedule
 import pywhatkit
+import os.path
 
 def acessa_suap(driver):
     driver.get('https://suap.ifsp.edu.br/accounts/login/?next=/')
@@ -23,30 +24,38 @@ def get_notas(driver):
     driver.get(f'https://suap.ifsp.edu.br/edu/aluno/{prontuario}/?tab=boletim')
     sleep(5)
     grade_table = driver.find_elements(By.CSS_SELECTOR, "tr[class^=' matricula_periodo_agrupamento']")
-    with open('notas.csv', 'r', encoding='utf-8') as arquivo_read:
-        notas = list(csv.reader(arquivo_read))
+    notas = None
+    if verifica_arquivo():  
+        with open('notas.csv', 'r', encoding='utf-8') as arquivo_read:
+            notas = list(csv.reader(arquivo_read))
+    else:
+        with open('notas.csv', 'w', encoding='utf-8', newline='') as arquivo_write:
+            w = csv.writer(arquivo_write)
+            for materia in grade_table:
+                linha = []
+                materia = materia.find_elements(By.TAG_NAME, 'td')
+                nome_materia = materia[2].get_attribute('innerHTML')
+                nome_materia = nome_materia[nome_materia.rfind('-') + 2: len(nome_materia) - 1]
+                linha.append(nome_materia)
+                for i in range(9, 17):
+                    if i % 2 != 0: # pega somente as notas
+                        linha.append(materia[i].get_attribute('innerHTML'))
+                if notas != None:
+                    if not linha in notas:
+                        envia_mensagem(f'A nota de {linha[0].title()} mudou!!')
 
-    with open('notas.csv', 'w', encoding='utf-8', newline='') as arquivo_write:
-        w = csv.writer(arquivo_write)
-        for materia in grade_table:
-            linha = []
-            materia = materia.find_elements(By.TAG_NAME, 'td')
-            nome_materia = materia[2].get_attribute('innerHTML')
-            nome_materia = nome_materia[nome_materia.rfind('-') + 2: len(nome_materia) - 1]
-            linha.append(nome_materia)
-            for i in range(9, 17):
-                if i % 2 != 0: # pega somente as notas
-                    linha.append(materia[i].get_attribute('innerHTML'))
-            if not linha in notas:
-                envia_mensagem(f'A nota de {linha[0].title()} mudou!!')
-
-            w.writerow(linha)
+                w.writerow(linha)
 
 
 def envia_mensagem(mensagem):
     id_grupo = config('ID_GRUPO')
     pywhatkit.sendwhatmsg_to_group_instantly(id_grupo, mensagem)
 
+
+def verifica_arquivo():
+    if os.path.isfile('notas.csv'):
+        return True
+    return False
 
 driver = webdriver.Chrome()
 acessa_suap(driver)
