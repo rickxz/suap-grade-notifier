08175f02be0e872b1d0586472293563a70f5f4a1
rickxz <herick.victor.rodrigues@hotmail.com>
2024-10-21 17:15:36 -0300

    feat(wip): implement mongodb connection to handle multitenancy

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from decouple import config
-from time import sleep
 import csv
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import schedule
-import pywhatkit
+from selenium.webdriver.remote.webdriver import WebDriver 
 import os.path
+from pymongo.mongo_client import MongoClient
 
-def acessa_suap(driver):
+
+client = MongoClient(config('MONGODB_URI'))
+
+def acessa_suap(driver: WebDriver):
     driver.get('https://suap.ifsp.edu.br/accounts/login/?next=/')
     username_input = driver.find_element(By.ID, 'id_username')
     username_input.send_keys(config('USER'))
@@ -26,36 +29,37 @@ def acessa_suap(driver):
         driver.find_element(By.XPATH, "/html/body/div[1]/a/span[1]").click()
 
 
-def get_notas(driver):
+def iterate_subjects(driver: WebDriver):
     prontuario = config('USER')
-    driver.get(f'https://suap.ifsp.edu.br/edu/aluno/{prontuario}/?tab=boletim')
+    driver.get(f'https://suap.ifsp.edu.br/edu/aluno/{prontuario.upper()}/?tab=boletim')
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "tr[class^=' matricula_periodo_agrupamento']")))
-    grade_table = driver.find_elements(By.CSS_SELECTOR, "tr[class^=' matricula_periodo_agrupamento']")
+    wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "table[id^='tabela_boletim'] > tbody tr")))
+    class_subjects = driver.find_elements(By.CSS_SELECTOR, "table[id^='tabela_boletim'] > tbody tr")
     notas = None
     if verifica_arquivo():  
         with open('notas.csv', 'r', encoding='utf-8') as arquivo_read:
             notas = list(csv.reader(arquivo_read))
-    with open('notas.csv', 'w', encoding='utf-8', newline='') as arquivo_write:
-        w = csv.writer(arquivo_write)
-        for materia in grade_table:
-            linha = []
-            materia = materia.find_elements(By.TAG_NAME, 'td')
-            nome_materia = materia[2].get_attribute('innerHTML')
-            nome_materia = nome_materia[nome_materia.rfind('-') + 2: len(nome_materia) - 1]
-            linha.append(nome_materia)
-            for i in range(9, 17):
-                if i % 2 != 0: # pega somente as notas
-                    linha.append(materia[i].get_attribute('innerHTML'))
-            if notas != None and len(notas) != 0:
-                if not linha in notas:
-                    envia_mensagem(f'A nota de {linha[0].title()} mudou!!')
-            w.writerow(linha)
+    for subject in class_subjects:
+        line = []
+        subject_columns = subject.find_elements(By.TAG_NAME, 'td')
+        subject_name = subject_columns[1].get_attribute('innerHTML')
+        subject_name = subject_name[subject_name.rfind('-') + 2: len(subject_name) - 1]
+        line.append(subject_name)
 
+        detail_button = subject_columns[-1].find_element(By.TAG_NAME, 'a')
+        detail_button.click()
+        get_grades(driver, subject_name)
+        driver.find_element(By.CSS_SELECTOR, "div.tclose").click()
 
-def envia_mensagem(mensagem):
-    id_grupo = config('ID_GRUPO')
-    pywhatkit.sendwhatmsg_to_group_instantly(id_grupo, mensagem)
+def get_grades(driver: WebDriver, subject_name):
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.tinner .borda:first-of-type > tbody > tr")))
+    grades_rows = driver.find_elements(By.CSS_SELECTOR, ".tinner .borda:first-of-type > tbody > tr")
+    for row in grades_rows:
+        columns = row.find_elements(By.TAG_NAME, 'td')
+        assessment_name = columns[0].get_attribute('innerHTML')
+        grade = columns[-1].get_attribute('innerHTML')
+        print(f'{subject_name} | {assessment_name}: {grade}')
 
 
 def verifica_arquivo():
@@ -65,9 +69,5 @@ def verifica_arquivo():
 
 driver = webdriver.Chrome()
 acessa_suap(driver)
-get_notas(driver)
-schedule.every(10).minutes.do(get_notas, driver)
-
-while True:
-    schedule.run_pending()
-    sleep(5)
+iterate_subjects(driver)
+driver.close()
