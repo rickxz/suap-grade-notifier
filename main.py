b75e2c171fd8a677c6d186d2e867214aa8cc3da6
rickxz <herick.victor.rodrigues@hotmail.com>
2024-10-22 20:38:00 -0300

    feat: retrieve grades from db, verify changes and update data

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from decouple import config
-import csv
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,19 +6,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.remote.webdriver import WebDriver 
-import os.path
 from pymongo.mongo_client import MongoClient
+from cryptography.fernet import Fernet
 
 
 client = MongoClient(config('MONGODB_URI'))
+fernet = Fernet(config("HASH_KEY").encode())
+users_collection = client['suap-grade-notifier']['user-grades']
+users = users_collection.find()
 
-def acessa_suap(driver: WebDriver):
+def acessa_suap(driver: WebDriver, prontuary: str, password: str):
     driver.get('https://suap.ifsp.edu.br/accounts/login/?next=/')
     username_input = driver.find_element(By.ID, 'id_username')
-    username_input.send_keys(config('USER'))
+    username_input.send_keys(prontuary)
 
     password_input = driver.find_element(By.ID, 'id_password')
-    password_input.send_keys(config('PASSWORD'))
+    password_input.send_keys(password)
     password_input.send_keys(Keys.ENTER)
     wait = WebDriverWait(driver, 10)
     try:
@@ -29,16 +30,13 @@ def acessa_suap(driver: WebDriver):
         driver.find_element(By.XPATH, "/html/body/div[1]/a/span[1]").click()
 
 
-def iterate_subjects(driver: WebDriver):
-    prontuario = config('USER')
-    driver.get(f'https://suap.ifsp.edu.br/edu/aluno/{prontuario.upper()}/?tab=boletim')
+def iterate_subjects(driver: WebDriver, prontuary: str):
+    driver.get(f'https://suap.ifsp.edu.br/edu/aluno/{prontuary.upper()}/?tab=boletim')
     wait = WebDriverWait(driver, 10)
     wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "table[id^='tabela_boletim'] > tbody tr")))
     class_subjects = driver.find_elements(By.CSS_SELECTOR, "table[id^='tabela_boletim'] > tbody tr")
-    notas = None
-    if verifica_arquivo():  
-        with open('notas.csv', 'r', encoding='utf-8') as arquivo_read:
-            notas = list(csv.reader(arquivo_read))
+
+    grades_dict = {}
     for subject in class_subjects:
         line = []
         subject_columns = subject.find_elements(By.TAG_NAME, 'td')
@@ -48,10 +46,15 @@ def iterate_subjects(driver: WebDriver):
 
         detail_button = subject_columns[-1].find_element(By.TAG_NAME, 'a')
         detail_button.click()
-        get_grades(driver, subject_name)
+        grades_dict[subject_name] = {}
+        get_grades(driver, subject_name, grades_dict)
         driver.find_element(By.CSS_SELECTOR, "div.tclose").click()
 
-def get_grades(driver: WebDriver, subject_name):
+    return grades_dict
+
+    
+
+def get_grades(driver: WebDriver, subject_name: str, grades_dict: dict):
     wait = WebDriverWait(driver, 10)
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.tinner .borda:first-of-type > tbody > tr")))
     grades_rows = driver.find_elements(By.CSS_SELECTOR, ".tinner .borda:first-of-type > tbody > tr")
@@ -60,14 +63,32 @@ def get_grades(driver: WebDriver, subject_name):
         assessment_name = columns[0].get_attribute('innerHTML')
         grade = columns[-1].get_attribute('innerHTML')
         print(f'{subject_name} | {assessment_name}: {grade}')
+        grades_dict[subject_name][assessment_name] = grade
 
 
-def verifica_arquivo():
-    if os.path.isfile('notas.csv'):
-        return True
-    return False
+def compare_grades(user_prontuary: str, previous_grades: dict, current_grades: dict):
+    if previous_grades == current_grades: return
 
-driver = webdriver.Chrome()
-acessa_suap(driver)
-iterate_subjects(driver)
-driver.close()
+    for subject_prev, subject_curr in zip(previous_grades, current_grades):
+        prev_grades = previous_grades[subject_prev]
+        curr_grades = current_grades[subject_curr]
+        for prev_grade, curr_grade in zip(prev_grades, curr_grades):
+            if prev_grades[prev_grade] != curr_grades[curr_grade]:
+                print('a nota mudou!')
+
+    filter = {'prontuary': user_prontuary }
+    new_values = {"$set": {'grades': current_grades}}
+
+    users_collection.update_one(filter, new_values)
+    print('usuario atualizado com sucesso!')
+
+for user in users:
+    driver = webdriver.Edge()
+    password_decrypted = fernet.decrypt(user['password'].encode()).decode()
+    acessa_suap(driver, user['prontuary'], password_decrypted)
+    current_grades_dict = {}
+    current_grades_dict = iterate_subjects(driver, user['prontuary'])
+    previous_grades_dict = user.get('grades') or {}
+    compare_grades(user['prontuary'], previous_grades_dict, current_grades_dict)
+ 
+    driver.close()
